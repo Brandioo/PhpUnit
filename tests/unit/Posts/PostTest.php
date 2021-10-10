e9ea2145dd257136dd6c5c88c0b0c2d8c9ada0bf
Brandioo <66759355+Brandioo@users.noreply.github.com>
2021-10-10 12:26:53 +0200

    Run Tests By Testing.txt

--- tests/unit/Posts/PostTest.php
+++ tests/unit/Posts/PostTest.php
@@ -1,38 +1,31 @@
 <?php
 
-use App\Models\Categories;
 use App\Models\Posts;
 
-class PostTest extends \PHPUnit\Framework\TestCase{
-    private Posts $post;
+class PostTest extends \PHPUnit\Framework\TestCase
+{
+    private $post = null;
 
     protected function setUp(): void
     {
-        $this->post = new Posts;
+        $this->post = new Posts();
     }
 
     /** @test */
-    public function shouldReturnPostTitle(){
-        $this->post->setTitle("My first post");
-
-        $this->assertEquals($this->post->getTilte(), "My first post");
+    public function shouldReturnPostTitle()
+    {
+        $this->post->setTitle("GoodWork");
+        $this->assertEquals($this->post->getTitle(), "GoodWork");
     }
 
     /** @test */
-    public function shouldReturnPostDescription(){
-        $this->post->setDescription("Test description");
-
-        $this->assertEquals($this->post->getDescription(), "Test description");
-    }
-
-    /** @test */
-    public function shouldReturnPostCategory(){
-        $category = new Categories;
-        $category->setName("Sports category");
-        $category->setFriendlyUrl("sports-category");
-
-        $this->post->setCategory($category);
-
-        $this->assertEquals($this->post->getCategory(), $category);
+    public function shouldReturnPost()
+    {
+        $this->post->setTitle("GoodWork");
+        $this->post->setCategory("Work");
+        $this->post->setDescription("Good");
+        $this->assertEquals($this->post->getTitle(), "GoodWork");
+        $this->assertEquals($this->post->getCategory(), "Work");
+        $this->assertEquals($this->post->getDescription(), "Good");
     }
 }
